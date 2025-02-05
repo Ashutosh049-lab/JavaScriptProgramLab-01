70e3eba134b1e881505c639ab19aa3b716cde785
Ashutoshkj <jashutosh528@gmail.com>
2025-02-06 00:03:28 +0530

    Happy Number

---
+++ HappyNumber.js
@@ -0,0 +1,23 @@
+
+function HappyNumber(N){
+
+    while(N>9){
+        let sum=0;
+
+        while(N>0){
+            let rem=N%10;
+            sum=sum+rem**2;
+            N=Math.floor(N/10);
+        }
+        N=sum;
+    }
+
+    if(N==1){
+        console.log("Happy Number");
+
+    }else{
+        console.log("Not Happy Number");
+    }
+}
+
+HappyNumber(19);
