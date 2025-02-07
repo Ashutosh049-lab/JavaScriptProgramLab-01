f3fd47f4ed4e2c151c57cf789fcc9fc1986dbb47
Ashutoshkj <jashutosh528@gmail.com>
2025-02-07 17:14:42 +0530

    Find Largest of Three Numbers Using Ternary Operator

--- largestNumber.js
+++ largestNumber.js
@@ -1,14 +1,14 @@
 
 function largest(num1,num2,num3){
 
-    
+
     return (num1 > num2) && (num1>num3) ? num1 : (num2> num1) && (num2 > num3) ? num2 :(num3 > num1) && (num3>num2) ? num3 :-1
   
 }
 
 
-  let num1=100;
-  let num2=200;
-  let num3=85;
+  let num1=45;
+  let num2=78;
+  let num3=33;
   let output=largest(num1,num2,num3)
   console.log(output)
