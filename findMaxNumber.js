6d0c0c875ea0a45e9d68074404d6659b1b61b0e0
Ashutoshkj <jashutosh528@gmail.com>
2025-02-13 21:57:50 +0530

    find Largest Number

---
+++ findMaxNumber.js
@@ -0,0 +1,14 @@
+
+function findMax(num1,num2,num3){
+    let max=num1;
+
+    if(num2>max){
+        max=num2;
+    }
+    if(num3>max){
+        max=num3;
+    }
+    console.log(max)
+}
+
+findMax(10,20,30);
