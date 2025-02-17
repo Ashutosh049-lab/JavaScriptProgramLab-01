a84905997acd20599d62f3a1f53c9c83d4f5b147
Ashutoshkj <jashutosh528@gmail.com>
2025-02-17 22:06:13 +0530

    Simple Interest Calculator

---
+++ Problem4_SimpleInterestCalculator.js
@@ -0,0 +1,11 @@
+
+function SimpleInterestCalculator(P,R,T){
+
+    let interest=(P*R*T)/100;
+    console.log(interest)
+}
+
+
+SimpleInterestCalculator(1000,5,3);
+SimpleInterestCalculator(1500,7,5);
+SimpleInterestCalculator(0,6,2);
