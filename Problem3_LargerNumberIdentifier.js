3294a9e748edcf4469948c1cea763a6ffcba37e1
Ashutoshkj <jashutosh528@gmail.com>
2025-02-17 21:50:01 +0530

    Larger Number Identifier

---
+++ Problem3_LargerNumberIdentifier.js
@@ -0,0 +1,15 @@
+
+function LargerNumberIdentifier(num1,num2){
+
+    if(num1==num2){
+        console.log("Both Numbers are equal");
+    }else if(num1 > num2){
+        console.log(num1+" is greater than "+num2);
+    }else if(num1 < num2){
+        console.log(num2+" is greater than "+num1);
+    }
+}
+
+LargerNumberIdentifier(20,10)
+LargerNumberIdentifier(7,7)
+LargerNumberIdentifier(-1,-10)
