44ea5f515659040a937fe0d0ab6d10fc4f11d079
Ashutoshkj <jashutosh528@gmail.com>
2025-02-24 22:41:42 +0530

    Handling Dynamic Adjustments

---
+++ HandlingDynamicAdjustments.js
@@ -0,0 +1,26 @@
+
+
+function HandlingDynamicAdjustments(arr){
+
+    for(let i=0;i<arr.length;i++){
+
+        if(arr[i]< 40){
+            arr[i]=arr[i]+20;
+        }else if(arr[i] >90){
+            arr[i]=90;
+        }
+    }
+
+    let count=0;
+
+    for(let i=0;i<arr.length;i++){
+        if(arr[i]>=50){
+            count++
+        }
+    }
+    console.log(count);
+}
+
+let arr=[10,20,30,40,50,60,70,80,90,100];
+
+HandlingDynamicAdjustments(arr);
