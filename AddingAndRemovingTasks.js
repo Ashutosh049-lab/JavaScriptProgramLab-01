f81bfb8d7438b0a53d9d6cd1374e4a447b7b52ba
Ashutoshkj <jashutosh528@gmail.com>
2025-02-26 23:39:00 +0530

    Managing a To-Do List: Adding and Removing Tasks

---
+++ AddingAndRemovingTasks.js
@@ -0,0 +1,13 @@
+
+function AddingAndRemovingTasks(arr){
+
+   // console.log(arr)
+    arr.unshift("newTask");
+   
+    arr.pop();
+    console.log(arr)
+
+}
+
+let arr=["Task1","Task2","Task3","Task4","Task5"];
+AddingAndRemovingTasks(arr)
