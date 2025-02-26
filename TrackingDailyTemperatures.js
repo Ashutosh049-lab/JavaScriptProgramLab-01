e6e5661aa2c611a8209821cd6830d22f0cff7c49
Ashutoshkj <jashutosh528@gmail.com>
2025-02-26 23:30:52 +0530

    Tracking Daily Temperatures

---
+++ TrackingDailyTemperatures.js
@@ -0,0 +1,11 @@
+
+function TrackingDailyTemperatures(arr){
+   
+  //console.log(arr[3]);
+  arr[3]=arr[3]+2;
+  console.log(arr)
+
+}
+
+let arr=[5,10,15,20,25,30,35]
+TrackingDailyTemperatures(arr);
