4c81e53396b559d1adbf0bdf9f8037d8f262eaff
Ashutoshkj <jashutosh528@gmail.com>
2025-02-27 19:15:41 +0530

    print All Possible String

---
+++ printAllPosibleSubArray.js
@@ -0,0 +1,22 @@
+
+function sub(arr){
+  
+    let bag=[];
+    for(let i=0;i<arr.length;i++){
+      let temp=[]
+      for(let j=i;j<arr.length;j++){
+       temp.push(arr[j]);
+       bag.push([...temp])
+       
+      }
+     
+      
+    }
+    console.log(JSON.stringify(bag));
+   
+
+    
+  }
+  
+  let arr=[1,2,3]
+  sub(arr);
