cdb6a118a2fb11af0afd36ebcea322d4ab6d1c2f
Ashutoshkj <jashutosh528@gmail.com>
2025-03-02 20:28:27 +0530

    equilibriumElement

--- equilibriumElement.js
+++ equilibriumElement.js
@@ -1,2 +1,31 @@
 
 
+    
+function equilibriumElement(arr){
+
+    for(let i=1;i<arr.length-1;i++){
+
+        let leftSum=0;
+
+        for(j=i-1;j>=0;j--){
+            leftSum +=arr[j]
+        }
+
+        
+        let rightSum=0;
+
+        for(j=i+1;j<arr.length;j++){
+            rightSum +=arr[j]
+        }
+
+        if(leftSum==rightSum){
+            return i;
+        }
+    }
+    return -1
+}
+
+let arr=[6,3,7,2,4,9,9];
+
+let res=equilibriumElement(arr);
+console.log(res)
